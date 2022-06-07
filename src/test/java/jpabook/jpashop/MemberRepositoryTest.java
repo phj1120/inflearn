4d07041bf62cf39ed7507f4992c2394153ee030e
phj1120 <hyeonj1998@naver.com>
2022-06-07 15:53:36 +0900

    2. 도메인 분석 설계 - 변경 전

--- src/test/java/jpabook/jpashop/MemberRepositoryTest.java
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop;
 
+import jpabook.jpashop.domain.Member;
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -21,7 +22,7 @@ public class MemberRepositoryTest {
     public void testMember() throws Exception {
         // given
         Member member = new Member();
-        member.setUsername("memberA");
+        member.setName("memberA");
 
         // when
         Long saveId = memberRepository.save(member);
@@ -29,7 +30,7 @@ public class MemberRepositoryTest {
 
         // then
         Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
-        Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        Assertions.assertThat(findMember.getName()).isEqualTo(member.getName());
 
         // 동일한 영속성 컨텍스트에서 id 가 같은 경우 실제 쿼리를 날리지 않고 1차 캐시에서 가져 옴
         Assertions.assertThat(findMember).isEqualTo(member);
