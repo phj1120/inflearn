f2fa7c2b6e3264999b717988429540bfc6633ede
phj1120 <hyeonj1998@naver.com>
2022-07-02 21:55:46 +0900

    타임리프-기본 기능

--- spring_mvc_2/thymeleaf-basic/src/main/java/hello/thymeleafbasic/basic/BasicController.java
+++ spring_mvc_2/thymeleaf-basic/src/main/java/hello/thymeleafbasic/basic/BasicController.java
@@ -99,15 +99,32 @@ public class BasicController {
     @GetMapping("/each")
     public String each(Model model) {
         addUsers(model);
-
         return "/basic/each";
     }
 
     @GetMapping("/condition")
     public String condition(Model model) {
         addUsers(model);
-
         return "/basic/condition";
+    }
+
+    @GetMapping("/comments")
+    public String comments(Model model) {
+        model.addAttribute("data", "Spring!");
+        return "basic/comments";
+    }
+
+    @GetMapping("/block")
+    public String block(Model model) {
+        addUsers(model);
+        return "/basic/block";
+    }
+
+    @GetMapping("/javascript")
+    public String javascript(Model model) {
+        model.addAttribute("user", new User("userA", 10));
+        addUsers(model);
+        return "basic/javascript";
     }
 
     private void addUsers(Model model) {
@@ -118,7 +135,6 @@ public class BasicController {
 
         model.addAttribute("users", list);
     }
-
 
     @Data
     static class User {
