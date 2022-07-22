a71603c6c85284bb3c82b54805f10bc84cdbebb6
phj1120 <hyeonj1998@naver.com>
2022-07-22 11:39:32 +0900

    getReference의 결과  타입 비교시  instance of 로 비교

--- ORM-JPA-BASIC/hellojpa/src/main/java/hellojpa/JpaMain.java
+++ ORM-JPA-BASIC/hellojpa/src/main/java/hellojpa/JpaMain.java
@@ -4,7 +4,6 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
-import java.time.LocalDateTime;
 
 public class JpaMain {
 
@@ -16,19 +15,29 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Member member = new Member();
-            member.setUsername("phj");
-            em.persist(member);
+            Member member1 = new Member();
+            member1.setUsername("phj");
+            em.persist(member1);
+
+            Member member2 = new Member();
+            member2.setUsername("phj");
+            em.persist(member2);
 
             em.flush();
             em.clear();
 
-//            Member findMember = em.find(Member.class, member.getId());
-            Member findMember = em.getReference(Member.class, member.getId());
-
+            Member findMember = em.find(Member.class, member1.getId());
+            System.out.println("findMember.getClass() = " + findMember.getClass());
             System.out.println("findMember = " + findMember);
             System.out.println("findMember.getClass() = " + findMember.getClass());
 
+            Member refMember = em.getReference(Member.class, member2.getId());
+            System.out.println("refMember.getClass() = " + refMember.getClass());
+            System.out.println("refMember = " + refMember);
+            System.out.println("refMember.getClass() = " + refMember.getClass());
+
+            System.out.println("findMember " + (findMember instanceof Member));
+            System.out.println("refMember " + (refMember instanceof Member));
 
             tx.commit();
         } catch (Exception e) {
