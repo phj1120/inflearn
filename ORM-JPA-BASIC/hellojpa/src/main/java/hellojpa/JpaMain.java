4c85b5c4fdb047935a4a960512fb10dd54bdf089
phj1120 <hyeonj1998@naver.com>
2022-07-28 00:06:53 +0900

    값 타입 : 임베디드 타입

--- ORM-JPA-BASIC/hellojpa/src/main/java/hellojpa/JpaMain.java
+++ ORM-JPA-BASIC/hellojpa/src/main/java/hellojpa/JpaMain.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.time.LocalDateTime;
 import java.util.List;
 
 public class JpaMain {
@@ -16,54 +17,12 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Member member1 = new Member();
-            member1.setUsername("member1");
-            em.persist(member1);
+            Address address = new Address("CITY", "STREET", "ZIPCODE");
+            Member member = new Member();
+            member.setUsername("PHJ");
+            member.setAddress(address);
+            em.persist(member);
 
-            Member member2 = new Member();
-            member2.setUsername("member2");
-            em.persist(member2);
-
-            Member member3 = new Member();
-            member3.setUsername("member3");
-            em.persist(member3);
-
-            // Member 에서 친구 관계를 수정하고 싶었으나. 그러면 Member 객체 수정시 Friend 테이블 수정 되는 현상 발생
-            // 유지보수가 어려워 지므로 객체 지향적으로 손해를 보더라도 외래키가 있는 곳에서 수정되도록 하라는 영한님 말씀을 듣고 이렇게 했음
-            Friendship friendship1to2 = new Friendship(member1, member2);
-            Friendship friendship2to1 = new Friendship(member2, member1);
-
-            Friendship friendship1to3 = new Friendship(member1, member3);
-            Friendship friendship3to1 = new Friendship(member3, member1);
-
-            em.persist(friendship1to2);
-            em.persist(friendship2to1);
-            em.persist(friendship1to3);
-            em.persist(friendship3to1);
-
-            em.flush();
-            em.clear();
-
-            Member findMember1 = em.find(Member.class, member1.getId());
-            List<Member> member1Friends = findMember1.selectFriends();
-            System.out.println("MEMBER1's FRIENDS");
-            for (Member member1Friend : member1Friends) {
-                System.out.println("member1Friend = " + member1Friend);
-            }
-
-            Member findMember2 = em.find(Member.class, member2.getId());
-            List<Member> member2Friends = findMember2.selectFriends();
-            System.out.println("MEMBER2's FRIENDS");
-            for (Member member2Friend : member2Friends) {
-                System.out.println("member2Friend = " + member2Friend);
-            }
-
-            Member findMember3 = em.find(Member.class, member3.getId());
-            List<Member> member3Friends = findMember3.selectFriends();
-            System.out.println("MEMBER3's FRIENDS");
-            for (Member member3Friend : member3Friends) {
-                System.out.println("member3Friend = " + member3Friend);
-            }
 
             tx.commit();
         } catch (Exception e) {
--- ORM-JPA-BASIC/hellojpa/src/main/java/hellojpa/Member.java
+++ ORM-JPA-BASIC/hellojpa/src/main/java/hellojpa/Member.java
@@ -1,11 +1,9 @@
 package hellojpa;
 
 import javax.persistence.*;
-import java.util.ArrayList;
-import java.util.List;
 
 @Entity
-public class Member extends BaseEntity {
+public class Member {
 
     @Id
     @GeneratedValue
@@ -15,52 +13,50 @@ public class Member extends BaseEntity {
     @Column(name = "name")
     private String username;
 
-    @ManyToOne
-    @JoinColumn(name = "TEAM_ID")
-    private Team team;
+    @Embedded
+    private Period period;
 
-    @OneToMany(mappedBy = "requester")
-    private List<Friendship> friendships = new ArrayList<>();
+    @Embedded
+    private Address address;
 
-    /*
-     * 친구 관계를 편하게 조회하려면 requester, target / target, requester
-     * 이렇게 두 번 저장해야 조회하기 편하다고 함.
-     * 그래서 한번에 두개 저장할 수 있게 하는 것을
-     * Member 에서 하고 싶었으나
-     * Member 가 연관관계 주인이 아니라 수정 불가능...
-     * -> Repository 에서 관리하면 되는 건가?
-     */
-//    public void addFriend(Member member) {
-//        Friend friend1 = new Friend(member, this);
-//        Friend friend2 = new Friend(this, member);
-//    }
+    public Member() {
+    }
 
-    public List<Member> selectFriends() {
-        List<Member> memberList = new ArrayList<>();
-        for (Friendship friendship : friendships) {
-
-            memberList.add(friendship.getRequester());
-        }
-        return memberList;
+    public Member(String username, Period period, Address address) {
+        this.username = username;
+        this.period = period;
+        this.address = address;
     }
 
     public Long getId() {
         return id;
     }
 
-    public String getUsername() {
-        return username;
+    public void setId(Long id) {
+        this.id = id;
     }
 
-    public Team getTeam() {
-        return team;
+    public String getUsername() {
+        return username;
     }
 
     public void setUsername(String username) {
         this.username = username;
     }
 
-    public void setTeam(Team team) {
-        this.team = team;
+    public Period getPeriod() {
+        return period;
+    }
+
+    public void setPeriod(Period period) {
+        this.period = period;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
     }
 }
