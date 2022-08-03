5148758c926095775a2322e10d7dd613df3ea92e
phj1120 <hyeonj1998@naver.com>
2022-08-04 00:12:28 +0900

    경로 표현식

--- ORM-JPA-BASIC/jpql/src/main/java/jpql/JpaMain.java
+++ ORM-JPA-BASIC/jpql/src/main/java/jpql/JpaMain.java
@@ -1,6 +1,7 @@
 package jpql;
 
 import javax.persistence.*;
+import java.util.Collection;
 import java.util.List;
 
 public class JpaMain {
@@ -18,66 +19,66 @@ public class JpaMain {
             team1.setName("mjc");
             em.persist(team1);
 
-            Team team2 = new Team();
-            team2.setName("4this");
-            em.persist(team2);
+            Member member1 = new Member();
+            member1.setUsername("phj");
+            member1.setAge(25);
+            member1.changeTeam(team1);
+            member1.setType(MemberType.ADMIN);
+            em.persist(member1);
 
-            for (int i = 0; i <= 4; i++) {
-                Member member = new Member();
-                member.setUsername("phj" + i);
-                member.setAge(i);
-                member.changeTeam(team1);
-                member.setType(MemberType.ADMIN);
-                em.persist(member);
-            }
-
-            for (int i = 5; i < 10; i++) {
-                Member member = new Member();
-                member.setAge(i);
-                member.changeTeam(team2);
-                member.setType(MemberType.USER);
-                em.persist(member);
-            }
+            Member member2 = new Member();
+            member2.setUsername("park");
+            member2.setAge(25);
+            member2.changeTeam(team1);
+            member2.setType(MemberType.ADMIN);
+            em.persist(member2);
 
             em.flush();
             em.clear();
 
             String query;
-            System.out.println("=====================================================================");
-            query = "select " +
-                    "case when m.age <= 10 then '학생요금' " +
-                    "     when m.age >= 60 then '경로요금' " +
-                    "     else '일반요금' " +
-                    "end " +
-                    "from Member m";
-            List<String> resultList1 = em.createQuery(query, String.class)
+            System.out.println("상태 필드 =====================================================================");
+            query = "select m.username from Member m";
+            System.out.println(query);
+            List<String> result1 = em.createQuery(query, String.class)
                     .getResultList();
-
-            for (String s : resultList1) {
-                System.out.println("s = " + s);
+            for (String username : result1) {
+                System.out.println("username = " + username);
             }
-            System.out.println("=====================================================================");
 
-            query = "select coalesce(m.username, '이름 없는 회원') as username " +
-                    "from Member m";
-            List<String> resultList2 = em.createQuery(query, String.class)
+            System.out.println("단일 값 연관 필드 =====================================================================");
+            query = "select m.team from Member m";
+            System.out.println(query);
+            List<Team> result2 = em.createQuery(query, Team.class)
                     .getResultList();
-
-            for (String s : resultList2) {
-                System.out.println("s = " + s);
+            for (Team team : result2) {
+                System.out.println("Team = " + team);
             }
-            System.out.println("=====================================================================");
 
-            query = "select nullif(m.username, :username) " +
-                    "from Member m";
-            List<String> resultList3 = em.createQuery(query, String.class)
-                    .setParameter("username", "phj0")
+            System.out.println();
+            query = "select t from Member m join m.team t";
+            System.out.println(query);
+            List<Team> result3 = em.createQuery(query, Team.class)
                     .getResultList();
-
-            for (String s : resultList3) {
-                System.out.println("s = " + s);
+            for (Team team : result3) {
+                System.out.println("Team = " + team);
             }
-            System.out.println("=====================================================================");
+
+            System.out.println();
+            query = "select m from Member m join m.team t";
+            System.out.println(query);
+            List<Member> result4 = em.createQuery(query, Member.class).getResultList();
+            for (Member member : result4) {
+                System.out.println("member = " + member);
+            }
+
+            System.out.println("컬렉션 값 연관 필드 =====================================================================");
+            query = "select t.members from Team t";
+            System.out.println(query);
+            List<Collection> result5 = em.createQuery(query, Collection.class).getResultList();
+            for (Object o : result5) {
+                System.out.println("o = " + o);
+            }
 
             tx.commit();
         } catch (Exception e) {
