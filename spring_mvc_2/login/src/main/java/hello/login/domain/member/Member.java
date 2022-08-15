dfcec33badc8a07837da08ac5a259d680ceb920d
phj1120 <hyeonj1998@naver.com>
2022-08-15 17:15:48 +0900

    회원 가입 기능 추가

---
+++ spring_mvc_2/login/src/main/java/hello/login/domain/member/Member.java
@@ -0,0 +1,22 @@
+package hello.login.domain.member;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.NotEmpty;
+
+@Getter
+@Setter
+public class Member {
+
+    private Long id;
+
+    @NotEmpty
+    private String loginId;
+
+    @NotEmpty
+    private String name;
+
+    @NotEmpty
+    private String password;
+}
