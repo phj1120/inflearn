0fc21ffb5b968dbfb6d8e478f0771288d54987f3
phj1120 <hyeonj1998@naver.com>
2022-10-11 00:07:13 +0900

    스프링 MVC2 : 스프링 부트 - 오류 페이지

--- spring_mvc_2/exception/src/main/java/hello/exception/servlet/ServletExController.java
+++ spring_mvc_2/exception/src/main/java/hello/exception/servlet/ServletExController.java
@@ -16,9 +16,29 @@ public class ServletExController {
         throw new RuntimeException("예외 발생");
     }
 
+    @GetMapping("/error-400")
+    public void error400(HttpServletResponse response) throws IOException {
+        response.sendError(400, "400 오류");
+    }
+
     @GetMapping("/error-404")
     public void error404(HttpServletResponse response) throws IOException {
         response.sendError(404, "404 오류");
+    }
+
+    @GetMapping("/error-405")
+    public void error405(HttpServletResponse response) throws IOException {
+        response.sendError(405, "405 오류");
+    }
+
+    @GetMapping("/error-300")
+    public void error300(HttpServletResponse response) throws IOException {
+        response.sendError(300, "300 오류");
+    }
+
+    @GetMapping("/error-301")
+    public void error301(HttpServletResponse response) throws IOException {
+        response.sendError(301, "301 오류");
     }
 
     @GetMapping("/error-500")
