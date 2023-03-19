4a983e33a12b6b37bbd0a8c58c882caa5069e554
phj1120 <hyeonj1998@naver.com>
2023-03-19 23:12:16 +0900

    필드 동기화 적용

---
+++ advanced/src/test/java/hello/advanced/app/trace/logtrace/FieldLogTraceTest.java
@@ -0,0 +1,30 @@
+package hello.advanced.app.trace.logtrace;
+
+import hello.advanced.app.trace.TraceStatus;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+class FieldLogTraceTest {
+
+    private LogTrace trace = new FieldLogTrace();
+
+    @Test
+    void begin_end_level2() {
+        TraceStatus status1 = trace.begin("hello1");
+        TraceStatus status2 = trace.begin("hello2");
+        trace.end(status2);
+        trace.end(status1);
+    }
+
+    @Test
+    void begin_end_level3() {
+        TraceStatus status1 = trace.begin("hello1");
+        TraceStatus status2 = trace.begin("hello2");
+        TraceStatus status3 = trace.begin("hello3");
+        trace.end(status3);
+        trace.end(status2);
+        trace.end(status1);
+    }
+
+}
