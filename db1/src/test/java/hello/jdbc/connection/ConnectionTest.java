17eedf98433d1003f89060acb080f87cd5c042be
phj1120 <hyeonj1998@naver.com>
2023-04-04 15:08:49 +0900

    트랜잭션 이해 실습

--- db1/src/test/java/hello/jdbc/connection/ConnectionTest.java
+++ db1/src/test/java/hello/jdbc/connection/ConnectionTest.java
@@ -1,15 +1,17 @@
 package hello.jdbc.connection;
 
+import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
+import hello.jdbc.domain.Member;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.jdbc.support.JdbcUtils;
 
 import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 import static hello.jdbc.connection.ConnectionConst.*;
 
@@ -20,6 +22,7 @@ public class ConnectionTest {
     void driverManager() throws SQLException {
         Connection con1 = DriverManager.getConnection(URL, USERNAME, PASSWORD);
         Connection con2 = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+
         log.info("connection={}, class={}", con1, con1.getClass());
         log.info("connection={}, class={}", con2, con2.getClass());
     }
@@ -33,7 +36,11 @@ public class ConnectionTest {
         DataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
 
         // 사용
-        useDataSource(dataSource);
+        Connection con0 = dataSource.getConnection();
+        Connection con1 = dataSource.getConnection();
+
+        log.info("connection={}, class={}", con0, con0.getClass());
+        log.info("connection={}, class={}", con1, con1.getClass());
     }
 
     @Test
@@ -103,5 +110,57 @@ public class ConnectionTest {
         Connection con17 = dataSource.getConnection();
         Connection con18 = dataSource.getConnection();
         Connection con19 = dataSource.getConnection();
+    }
+
+    void hikariReturnConnection(DataSource dataSource) throws SQLException {
+        Connection con = dataSource.getConnection();
+        log.info("Connection: {} class: {}", con, con.getClass());
+        String Sql = "select * from member";
+        PreparedStatement pstmt = con.prepareStatement(Sql);
+        ResultSet rs = pstmt.executeQuery();
+
+
+        List<Member> memberList = new ArrayList<>();
+        while (rs.next()) {
+            String memberId = rs.getString("member_id");
+            int money = rs.getInt("money");
+            memberList.add(new Member(memberId, money));
+        }
+        log.info(memberList.toString());
+
+        JdbcUtils.closeResultSet(rs);
+        JdbcUtils.closeStatement(pstmt);
+        JdbcUtils.closeConnection(con);
+    }
+
+    @Test
+    void hikariReturnConnectionRepeat() throws SQLException, InterruptedException {
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(URL);
+        dataSource.setUsername(USERNAME);
+        dataSource.setPassword(PASSWORD);
+        dataSource.setMaximumPoolSize(10);
+        dataSource.setConnectionTimeout(5000L);
+
+        Thread.sleep(1000);
+
+        hikariReturnConnection(dataSource);
+        hikariReturnConnection(dataSource);
+        hikariReturnConnection(dataSource);
+        hikariReturnConnection(dataSource);
+
+        Thread.sleep(1000);
+    }
+
+    @Test
+    void dataSourceReturnConnectionRepeat() throws SQLException, InterruptedException {
+        DataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
+
+        hikariReturnConnection(dataSource);
+        hikariReturnConnection(dataSource);
+        hikariReturnConnection(dataSource);
+        hikariReturnConnection(dataSource);
+
+        Thread.sleep(1000);
     }
 }
